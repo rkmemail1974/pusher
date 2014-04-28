d369b60224eed3841780af17c626ca859216b753
Robert Moon <rkm_email@yahoo.com>
2014-04-28 15:00:22 -0700

    pusher

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pusher::Application.config.secret_key_base = 'd0eeeb8c4365b74f6cd8b75b98551734889ba4244f2db5011ba5ff8a34eaa552a058075cfe60dca66a4d4c75d27f1701ef3ffb032438d3ce5b99db8b94edb98e'

ac00cc2aa36c52895f09e376a580ff7f4e9ec168
Robert Moon <rkm_email@yahoo.com>
2014-04-28 17:39:05 -0700

    fixed pusher

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,4 +5,8 @@ class WelcomeController < ApplicationController
         Pusher['test_channel'].trigger('greet', {
             :greeting => "Hello there!"})
     end
+    
+    def binding
+        Pusher['test_channel'].trigger('binding', { :message => "good day"})
+    end
 end
